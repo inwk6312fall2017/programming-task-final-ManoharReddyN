b0b54ac7f4f57a1bf3d92e53987032c9b72e2749
ManoharreddyN <manohar.reddynarapareddy@dal.ca>
2017-10-20 19:05:04 +0000

    task

--- finaltask3.py
+++ finaltask3.py
@@ -2,5 +2,15 @@
 
 config=open('running-config.cfg','r')
 newconfig=open('newfile.cfg','w')
+f1 = open('text.cfg','w')
 for line in config:
    newconfig.write(line.replace('172','192'))
+
+list=[] 
+for line in config:
+  if 'access-list' in line:
+    print(line)
+    f.write(line)  
+
+config.close()
+
